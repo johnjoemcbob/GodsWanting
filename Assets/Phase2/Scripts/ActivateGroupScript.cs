65902e38e77d893bd6e8e49263db73c7851a227c
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-07-26 19:28:22 +0100

    Phase2 - Activatable Cleanup

---
+++ Assets/Phase2/Scripts/ActivateGroupScript.cs
@@ -0,0 +1,25 @@
+﻿// Matthew Cormack (@johnjoemcbob / www.matthewcormack.co.uk)
+// 26/07/16
+//
+// The Gods Are Wanting
+//
+// Activate Group Script
+// Activate other Activables all at once
+//
+
+public class ActivateGroupScript : ActivatableScript
+{
+	public ActivatableScript[] Activatables;
+
+	public override bool OnActivate()
+	{
+		if ( !base.OnActivate() ) return false;
+
+		foreach ( ActivatableScript activatable in Activatables )
+		{
+			activatable.OnActivate();
+        }
+
+		return true;
+	}
+}
