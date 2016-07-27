a6a6456a79f4a5c25fdb5fc8e291055d9e94fc34
Dziek Dyes-Bolt <hackingnasatips@gmail.com>
2016-07-27 01:56:24 +0100

    PhaseDelta: New arms.
    Can pick up arms
    Arms can twitch (if you turn motor on in Upper)
    Arms fully physics driven now
    Mad Hype

---
+++ Assets/PhaseDelta/Connector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Connector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.name == "CollectRadius")
+		{
+			
+		}
+	}
+	
+	public void Grabbed () {
+		GetComponent<Rigidbody>().isKinematic = true;
+	}
+}
---
+++ Assets/PhaseDelta/Grabber.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabber : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		
+		Connector c = other.gameObject.GetComponent<Connector>();
+	
+		if (c != null)
+		{
+			other.transform.SetParent(transform);
+			
+			c.Grabbed();
+			
+			float newX = Random.Range(-0.5f, 0.5f);
+			float newZ = 0.6f - Mathf.Abs(newX);
+			
+			newZ = Random.Range(0, 2) == 0 ? newZ : -newZ;
+			
+			// other.transform.localPosition = new Vector3(newX, 0, newZ);
+			other.transform.localPosition = new Vector3(0.6f, 0, 0);
+			
+		}
+	}
+}
