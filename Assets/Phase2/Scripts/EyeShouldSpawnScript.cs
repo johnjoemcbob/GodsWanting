952005f0c8748230d0ecc92be1f481c3015a8a84
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-08-02 12:44:16 +0100

    Phase2 - Random Eyes :.:.':)==

---
+++ Assets/Phase2/Scripts/EyeShouldSpawnScript.cs
@@ -0,0 +1,34 @@
+﻿// Matthew Cormack (@johnjoemcbob / www.matthewcormack.co.uk)
+// 02/08/16
+//
+// The Gods Are Wanting
+//
+// Eye Should Spawn Script
+// Toggle possible eyes based on chance and
+// if there is no collision
+//
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EyeShouldSpawnScript : MonoBehaviour
+{
+	public List<Collider> Ignore;
+
+	void Start()
+	{
+		bool collision = false;
+		foreach ( Collider col in Physics.OverlapSphere( transform.position, transform.localScale.x ) )
+		{
+			if  ( !Ignore.Contains( col ) )
+			{
+				collision = true;
+				break;
+			}
+		}
+		if ( collision || ( Random.Range( 0, 10 ) <= 7 ) )
+		{
+			gameObject.SetActive( false );
+		}
+	}
+}
